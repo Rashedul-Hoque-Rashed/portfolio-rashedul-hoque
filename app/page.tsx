02acce7da9f77cbdd121b4ded5ca7e7386d74cbb
Rashed <rashedulhoqueceo@gmail.com>
2023-12-11 22:45:31 +0600

    complated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Hero from '@/components/main/Hero'
-import Image from 'next/image'
+import Skills from '@/components/main/Skills'
+import Encryption from '@/components/main/Encryption'
+import Projects from '@/components/main/Projects'
+import Footer from '@/components/main/Footer'
+
 
 export default function Home() {
   return (
     <main className='w-full h-full'>
       <div className='flex flex-col h-[850px] gap-20'>
-<Hero/>
+        <Hero />
+        <Skills />
+        <Encryption/>
+        <Projects/>
+        <Footer/>
       </div>
     </main>
   )
